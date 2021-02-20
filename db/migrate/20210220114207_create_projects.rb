cc3e2610cabbfe1eb960e31c859ebf987e809a25
euangt <euan.gillespie@outlook.com>
2021-02-20 11:42:21 +0000

    projects model created

---
+++ db/migrate/20210220114207_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.references :portfolio, null: false, foreign_key: true
+      t.string :name
+      t.text :description
+      t.text :github_url
+      t.boolean :deployed
+      t.text :url
+
+      t.timestamps
+    end
+  end
+end
