f559e6df1f87cf57b7c5206fe12c0489be224dd5
euangt <euan.gillespie@outlook.com>
2021-02-20 16:34:54 +0000

    portfolio methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,15 @@
 class PortfoliosController < ApplicationController
+  before_action :find_portfolio, only: [ :show ]
+  
+  def index 
+  end
+
+  def show
+  end
+
+  private 
+
+  def find_portfolio
+    @portfolio = Portfolio.find(params[:id])
+  end
 end
