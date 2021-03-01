e2f609ab74135ae212fb6237a7395512d77fdf99
euangt <euan.gillespie@outlook.com>
2021-03-01 14:10:39 +0000

    skeleten controller methods

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,34 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :update, :destroy]
+  def index
+    @projects = Project.where(Project.user )
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private 
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+
+  end
 end
