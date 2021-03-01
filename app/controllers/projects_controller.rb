b2ce9a05665cc75148f5008827bb12880f509a3b
euangt <euan.gillespie@outlook.com>
2021-03-01 14:19:56 +0000

    project controller actions finished

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,25 +1,37 @@
 class ProjectsController < ApplicationController
   before_action :find_project, only: [:show, :update, :destroy]
   def index
-    @projects = Project.where(Project.user )
+    # Inital thought
+    @projects = Portfolio.find(params[:id]).projects.all
   end
 
   def show
   end
 
   def new
+    @project = Project.new
   end
 
   def create
-
+    @project = Project.new(project_params)
+    @project.user = current_user
+    @project.portfolio = Portfolio.find(params[:id])
+    if @project.save!
+      redirect_to @project
+    else 
+      render :new
+    end
   end
 
   def update
-
+    @project.update(project_params)
+    redirect_to @project
   end
 
   def destroy
-
+    @portfolio = Porfolio.find(params[:id])
+    @project.destroy
+    redirect_to @portfolio
   end
 
   private 
@@ -29,6 +41,6 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-
+    params.require(:project).permit(:name, :description, :github_url, :deployed, :url)
   end
 end
